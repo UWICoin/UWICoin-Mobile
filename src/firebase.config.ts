404fe08fc1496efdf3a92694746296e98bea987f
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-06 01:42:47 -0400

    Updated firebase config

---
+++ src/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyDDa13PqQMf8GxtSJnxSNUEVh46MXfUX_0',
+    authDomain: 'uwicoin.firebaseapp.com',
+    databaseURL: 'https://uwicoin.firebaseio.com',
+    projectId: 'uwicoin',
+    storageBucket: 'uwicoin.appspot.com',
+    messagingSenderId: '233823536584'
+}
