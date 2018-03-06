6babe2e88076aab79cf76f229a097f96961cc20b
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-06 05:28:30 -0400

    Updated signup page design

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,9 @@
+import { AuthenticationProvider } from './../../providers/authentication/authentication';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { emailDomainValidator, emailMatchValidator } from '../../validators/authentication/email/email-validator';
+import { passwordMatchValidator } from '../../validators/authentication/password/password-validator';
 
 @IonicPage()
 @Component({
@@ -15,11 +12,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  signupForm: FormGroup;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public authProvider: AuthenticationProvider,
+    public formBuilder: FormBuilder) {
+
+    this.signupForm = this.formBuilder.group({
+      'fullName': [null, Validators.compose([Validators.required, Validators.pattern('([A-Za-z]+[" "][A-Za-z]+)')])],
+      'email': [null, Validators.compose([Validators.required, Validators.email, emailDomainValidator, emailMatchValidator])],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(7)])],
+      'confirmPassword': [null, Validators.compose([Validators.required, passwordMatchValidator])]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  ionViewCanEnter() {
+    return !this.authProvider.isAuthenticated();
+  }
+
+  goToLogin() {
+    this.navCtrl.setRoot('LoginPage');
+  }
+
+  signup(user: any) {
+    this.authProvider.signup(user).then(result => {
+
+      if (this.authProvider.isAuthenticated()) {
+        console.log('The user is authenticated');
+        this.navCtrl.setRoot('DashboardPage');
+      }
+      else {
+        console.log('The user is not authenticated');
+        this.authProvider.logout().then(() => {
+          this.navCtrl.setRoot('LoginPage').then(() => {
+            // Show toast to verify email to continue
+          });
+        })
+      }
+    }, error => {
+      this.showError(error);
+    });
+  }
+
+  showError(error) {
+    console.error(error);
   }
 
 }
