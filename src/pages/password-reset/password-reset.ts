e4161f338f915ef879fd61b8165d86870331163a
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-06 17:35:28 -0400

    Udated password reset page

--- src/pages/password-reset/password-reset.ts
+++ src/pages/password-reset/password-reset.ts
@@ -1,12 +1,7 @@
+import { AuthenticationProvider } from './../../providers/authentication/authentication';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PasswordResetPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PasswordResetPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  resetForm: FormGroup;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public authProvider: AuthenticationProvider) {
+
+    this.resetForm = this.formBuilder.group({
+      'email': [null, Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  ionViewCanEnter(){
+    return !this.authProvider.isAuthenticated();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PasswordResetPage');
+  }
+
+  resetPassword(user: any) {
+    this.authProvider.forgotPassword(user.email).then(result => {
+      this.navCtrl.pop().then(() => {
+        // Show toast to please check you email
+      });
+    }, error => {
+      this.showError(error);
+    });
+  }
+
+  showError(error) {
+    console.error(error);
   }
 
 }
