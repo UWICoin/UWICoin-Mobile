015fbe248f15fecbe61a38b949f2ed6ad6a992eb
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-16 12:47:52 -0400

    Update user model

--- src/models/user/user.models.ts
+++ src/models/user/user.models.ts
@@ -1,9 +1,12 @@
 import { Roles } from "../roles/roles.models";
 
 export interface User {
+    account_setup?: boolean;
+    addresses?: any;
     full_name?: string;
     email?: string;
     password?: string;
+    photoURL?: string;
     uid?: string;
-    role: Roles;
+    role?: Roles;
 }
