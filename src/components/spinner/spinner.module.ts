63ac2a15e647d255add33a2c481bb0ac29382352
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-16 12:48:42 -0400

    Added spinner component

---
+++ src/components/spinner/spinner.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SpinnerComponent } from './spinner';
+
+@NgModule({
+	declarations: [SpinnerComponent],
+	imports: [],
+	exports: [SpinnerComponent]
+})
+export class SpinnerModule {}
---
+++ src/components/spinner/spinner.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'spinner',
+  templateUrl: 'spinner.html'
+})
+export class SpinnerComponent implements OnChanges {
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+
+}
