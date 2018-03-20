ab325a41d65df5ca4284a41126bde4de33efd49f
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-20 02:35:58 -0400

    Update user summary to use async objects

--- src/components/user-summary/user-summary.ts
+++ src/components/user-summary/user-summary.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { IBalance } from './../../models/balance/balance.models';
+import { AuthenticationProvider } from './../../providers/authentication/authentication';
+import { Observable } from 'rxjs/Observable';
+import { User } from './../../models/user/user.models';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'user-summary',
   templateUrl: 'user-summary.html'
 })
-export class UserSummaryComponent {
+export class UserSummaryComponent implements OnChanges {
 
-  fullName: string;
-  balance: number;
-  photoURL: string;
+  @Input('user')
+  user$: Observable<User>;
 
-  constructor() {
-    this.fullName = 'John Doe';
-    this.balance = 121.61;
-    this.photoURL = 'assets/imgs/avatar.png';
+  @Input('balance')
+  balance: Promise<IBalance>;
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    
   }
 
 }
