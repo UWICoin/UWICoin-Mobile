1151ab8f1c4470164e116aeb9bde52de558c9700
Darion Hernandez <darionhernandez868@gmail.com>
2018-03-20 02:36:54 -0400

    Updated components module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { UserSummaryComponent } from './user-summary/user-summary';
+import { CommonModule } from '@angular/common';
 @NgModule({
 	declarations: [UserSummaryComponent],
-	imports: [],
+	imports: [
+		CommonModule
+	],
 	exports: [UserSummaryComponent]
 })
 export class ComponentsModule {}
