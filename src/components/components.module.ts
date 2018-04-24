93a211dae86a6a5cff86d787cb2acdd526755ac4
Darion Hernandez <darionhernandez868@gmail.com>
2018-04-23 20:05:49 -0400

    Updated components as an ionic module. Allows components to use ionic components

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,12 +1,18 @@
+import { IonicModule } from 'ionic-angular';
 import { NgModule } from '@angular/core';
 import { UserSummaryComponent } from './user-summary/user-summary';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-	declarations: [UserSummaryComponent],
-	imports: [
-		CommonModule
+	declarations: [
+		UserSummaryComponent
 	],
-	exports: [UserSummaryComponent]
+	imports: [
+		CommonModule,
+		IonicModule
+	],
+	exports: [
+		UserSummaryComponent
+	]
 })
 export class ComponentsModule {}
